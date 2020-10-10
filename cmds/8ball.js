201f86d2cfafed4e4ae05bfb4edcfd3d8578bee5
Guin8110 <72637482+Guin8110@users.noreply.github.com>
2020-10-10 18:50:32 +0800

    Added 8Ball Cmd

---
+++ cmds/8ball.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message, args) => {
+	let answers = [
+	"Yes",
+	"No",
+	"Maybe",
+	"I Think"
+	] //You Can Add More!
+	
+	let rand = answers[Math.floor(Math.random() * answers.length)]
+	
+	let question = args.slice(1).join(" ")
+	if(!question) return message.channel.send("Please Input Some Text You Want To Ask")
+	
+	let embed = new Discord.MessageEmbed()
+	.setTitle("🎱 8Ball")
+	.setDescription(`**Question**: ${question}\n**Answer**:${rand}`)
+	.setColor("RANDOM")
+	
+	message.channel.send(embed)
+}
+
+module.exports.help = {
+	name: "8ball",
+	aliases: ["ask", "magic8ball"] //we put aliases here it means you can use them too!
+}
